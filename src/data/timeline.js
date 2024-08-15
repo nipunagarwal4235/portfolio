47274157f9dcddc35ec7356818d3e63f0a4acdd0
Nipun Agarwal <nipunagarwal322@gmail.com>
2024-08-16 01:04:33 +0530

    updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,15 +1,31 @@
 export default [
   {
+    year: "2024",
+    title: "Internship",
+    duration: "3 months",
+    details:
+      "I started by working on a few landing pages for a client, where I was introduced to MDX and Next.js. These tools made publishing and maintaining blogs significantly easier and more efficient. Next, I delved into SEO for a course on Frontend Hire. I thoroughly studied the Google SEO documentation, which enhanced my understanding of search engine optimization and its critical role in web development. The most exciting part of the internship was working on a React Native client project, where I developed an ecommerce app powered by Expo and Supabase. This hands-on experience was invaluable and taught me the intricacies of mobile app development. It was here that I discovered React Query, a library that instantly became one of my favorites due to its powerful data fetching capabilities. Following the app development, we transitioned to creating an admin dashboard using Next.js.",
+  },
+  {
+    year: "2024",
+    title: "Internship",
+    duration: "5 months",
+    details:
+      "As an intern at Sabudh Foundation, I had the opportunity to dive deep into the analysis of public sentiment surrounding the Farmers Protest in India. My work involved analyzing thousands of tweets to understand the prevailing sentiments and how social media reflects broader public opinion. Through this project, I honed my skills in data cleaning, analysis, and visualization, which provided valuable insights for media, government, and social platforms. It was an enriching experience that has greatly expanded my understanding of social media dynamics and public discourse.",
+  },
+  {
     year: "2023",
     title: "Freelance Project",
     duration: "4 months",
-    details: "I had completed a website development project for ACIC-BMU Foundation our university's incubation centre. The website was developed using ReactJS, Tailwind CSS, and other modern technologies to create a user-friendly and visually appealing platform for the incubation centre.",
+    details:
+      "I had completed a website development project for ACIC-BMU Foundation our university's incubation centre. The website was developed using ReactJS, Tailwind CSS, and other modern technologies to create a user-friendly and visually appealing platform for the incubation centre.",
   },
   {
     year: "2022",
     title: "Summer Internship",
     duration: "2 months",
-    details: "I had completed a two-month internship in the development of ERP software and digitalization of the startup ecosystem. I had the opportunity to work with a talented team of developers and gain hands-on experience in designing and implementing software solutions. This internship has further solidified my passion for technology and its impact on businesses.",
+    details:
+      "I had completed a two-month internship in the development of ERP software and digitalization of the startup ecosystem. I had the opportunity to work with a talented team of developers and gain hands-on experience in designing and implementing software solutions. This internship has further solidified my passion for technology and its impact on businesses.",
   },
   {
     year: "2021",
